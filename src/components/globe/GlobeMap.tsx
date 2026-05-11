c0f74748f6d19c5d863d5bc7c4f7ea96f4d229b3
AndreaMarangione <marangione9@gmail.com>
2026-05-11 21:33:49 +0200

    in progress

--- src/components/globe/GlobeMap.tsx
+++ src/components/globe/GlobeMap.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import {useEffect, useRef} from "react";
 import dynamic from "next/dynamic";
+import type {GlobeMethods} from "react-globe.gl";
 
 const Globe3D = dynamic(
     () => import("react-globe.gl"),
@@ -10,14 +12,43 @@ const Globe3D = dynamic(
 );
 
 const GlobeMap = () => {
+    const globeRef = useRef<GlobeMethods | undefined>(undefined);
+
+    useEffect(() => {
+
+        let animationFrame: number;
+
+        const animate = () => {
+
+            if (globeRef.current) {
+
+                globeRef.current
+                    .scene()
+                    .rotation.y += 0.0015;
+            }
+
+            animationFrame =
+                requestAnimationFrame(animate);
+        };
+
+        animate();
+
+        return () =>
+            cancelAnimationFrame(animationFrame);
+
+    }, []);
+
     return (
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center pointer-events-none">
+
             <Globe3D
+                ref={globeRef}
                 width={320}
                 height={320}
                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
                 backgroundColor="rgba(0,0,0,0)"
             />
+
         </div>
     );
 };
