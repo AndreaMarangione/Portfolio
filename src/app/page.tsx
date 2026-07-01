158f50a30ee5860d80c9d6da874c0d727287c12c
AndreaMarangione <marangione9@gmail.com>
2026-07-01 11:30:44 +0200

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import About from "@/components/sections/About";
 import Skills from "@/components/sections/Skills";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
+import SectionDivider from "@/components/sectionDivider/SectionDivider";
 
 export default function Home() {
     return (
@@ -13,28 +14,35 @@ export default function Home() {
                 </div>
             </section>
 
-            <section id="about" className="min-h-screen pt-12 scroll-mt-16">
+            <section id="about" className="min-h-screen pt-1 scroll-mt-16">
                 <div className="mx-auto w-full max-w-[1600px] px-6 lg:px-12">
+                    <SectionDivider dir="about"/>
                     <About/>
                 </div>
             </section>
 
             <section id="skills"
-                     className="min-h-screen scroll-mt-16 min-[1025px]:flex min-[1025px]:min-h-[calc(100vh_-_4rem)] min-[1025px]:flex-col min-[1025px]:justify-center"
+                     className="min-h-screen pt-1 scroll-mt-16 min-[1025px]:relative min-[1025px]:flex min-[1025px]:min-h-[calc(100vh_-_4rem)] min-[1025px]:flex-col min-[1025px]:justify-center"
             >
+                <div
+                    className="mx-auto w-full max-w-[1600px] px-6 lg:px-12 min-[1025px]:absolute min-[1025px]:left-1/2 min-[1025px]:top-0 min-[1025px]:-translate-x-1/2 min-[1025px]:pt-1">
+                    <SectionDivider dir="skills"/>
+                </div>
                 <div className="mx-auto w-full max-w-[1600px] px-6 lg:px-12">
                     <Skills/>
                 </div>
             </section>
 
-            <section id="projects" className="min-h-screen pt-12 scroll-mt-16">
+            <section id="projects" className="min-h-screen pt-1 scroll-mt-16">
                 <div className="mx-auto w-full max-w-[1600px] px-6 lg:px-12">
+                    <SectionDivider dir="projects"/>
                     <Projects/>
                 </div>
             </section>
 
-            <section id="contact" className="min-h-screen pt-12 scroll-mt-16">
+            <section id="contact" className="min-h-screen pt-1 scroll-mt-16">
                 <div className="mx-auto w-full max-w-[1600px] px-6 lg:px-12">
+                    <SectionDivider dir="contact"/>
                     <Contact/>
                 </div>
             </section>
