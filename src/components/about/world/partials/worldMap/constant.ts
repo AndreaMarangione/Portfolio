abe8d7eb24dda201bfdea2f8668e499fe98aa3ec
Andrea Marangione <marangione9@gmail.com>
2026-08-22 12:49:21 +0200

    world map ok

--- src/components/about/world/partials/worldMap/constant.ts
+++ src/components/about/world/partials/worldMap/constant.ts
@@ -1,26 +1,25 @@
-export const routes = [
-    {
-        id: "florence-tongxiang",
-        path: "M 1045 230 Q 1350 150 1635 318",
-    },
-    {
-        id: "balikesir-fuzhou",
-        path: "M 1135 260 Q 1455 210 1610 355",
-    },
-    {
-        id: "florence-houston",
-        path: "M 1045 230 Q 650 100 468 300",
-    },
-    {
-        id: "neumünster-andalusia",
-        path: "M 1045 160 Q 650 110 510 290",
-    },
-    {
-        id: "zaragoza-rudong",
-        path: "M 970 230 Q 1350 110 1620 290",
-    },
-    {
-        id: "rudong-izmit",
-        path: "M 1620 290 Q 1450 180 1145 235",
-    },
+export type WorldCity = {
+    name: string;
+    x: number;
+    y: number;
+    labelX: number;
+    labelY: number;
+};
+
+export const PULSE_MIN_RADIUS = 5;
+export const PULSE_MAX_RADIUS = 34;
+export const PULSE_DURATION_S = 2.6;
+export const PULSE_STAGGER_S = 0.26;
+
+export const cities: WorldCity[] = [
+    {name: "Tongxiang", x: 1640, y: 320, labelX: 1655, labelY: 340},
+    {name: "Rudong", x: 1620, y: 290, labelX: 1630, labelY: 285},
+    {name: "Fuzhou", x: 1610, y: 350, labelX: 1610, labelY: 385},
+    {name: "Balikesir", x: 1140, y: 260, labelX: 1150, labelY: 285},
+    {name: "Izmit", x: 1155, y: 235, labelX: 1160, labelY: 225},
+    {name: "Zaragoza", x: 970, y: 235, labelX: 860, labelY: 265},
+    {name: "Florence", x: 1050, y: 230, labelX: 1020, labelY: 215},
+    {name: "Neumünster", x: 1040, y: 160, labelX: 1040, labelY: 145},
+    {name: "Houston", x: 470, y: 300, labelX: 375, labelY: 330},
+    {name: "Andalusia", x: 515, y: 290, labelX: 510, labelY: 275},
 ];
